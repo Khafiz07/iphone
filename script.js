fa126da0a9dcaadfdbcda4b40b116b49c5f3233d
khafizdev <113028744+Khafiz07@users.noreply.github.com>
2022-09-18 23:09:05 +0500

    add site

---
+++ script.js
@@ -0,0 +1,20 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+
+
+   
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+      
+
+    var time = h + ":" + m  ;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000)
+}
+
+showTime();
